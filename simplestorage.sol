f97b35092f1f83f3d184618cc6572170bd945764
rafeedamv123-rgb <rafeedamv123@gmail.com>
2026-01-04 19:55:42 +0000

    adding solidity

---
+++ simplestorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract SimpleStorage {
+    uint256 public value;
+    function SetValue(uint256 _value) public {
+        value=_value;
+    }
+    function GetValue() public view returns (uint256){
+        return value;
+    }
+    }
