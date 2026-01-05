e1ba1e4bd234b2ac3484727ddab18845428c000d
rafeedamv123-rgb <rafeedamv123@gmail.com>
2026-01-05 17:01:08 +0000

    changing contract address

--- constant.js
+++ constant.js
@@ -4,7 +4,7 @@
 import { getAddress } from "https://esm.sh/viem";
 
 export const contractaddress = getAddress(
-  "0x8464135c8F25Da09e49BC8782676a84730C318bC"
+  "0x71C95911E9a5D330f4D621842EC243EE1343292e"
 );
 
 export const abi = [
